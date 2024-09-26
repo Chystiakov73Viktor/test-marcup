fa59a18aa00dad853b5e3e8cddeb61edb843312a
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-09-26 11:36:53 +0300

    deleted file

--- src/js/refs.js
+++
@@ -1,6 +0,0 @@
-export const refs = {
-    formEl: document.querySelector('.search-form'),
-    cardContainerEl: document.querySelector('.gallery'),
-    loadMore: document.querySelector('.load-more'),
-  }
-  
